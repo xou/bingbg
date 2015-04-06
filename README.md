d7522d30be6fe2d56b4dbb32158ce43fad3abc4f
Niko Weh <niko.weh@gmail.com>
2015-04-06 20:14:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+bingbg
+======
+
+Changes the desktop image to the current image on the bing.com
+homepage.
+
+Change set_image_cmd() to a background setter of your choice, default
+uses feh.
---
+++ bingbg.py
@@ -0,0 +1,46 @@
+import json
+import urllib
+import urllib2
+import tempfile
+import subprocess
+
+def set_image_cmd(filename):
+    return ['/usr/bin/feh', '--bg-scale', filename]
+
+API_URL = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
+
+def get_current_image_data():
+    """return the latest bing image data: A dictionary
+    contining the keys 'url' and 'description' at least.
+    url is relative to http(s)://bing.com/"""
+    req = urllib2.urlopen(API_URL)
+    data = req.read()
+    req.close()
+    # returned data is a list of images, choose the first one
+    data = json.loads(data)['images'][0]
+    return data
+
+
+def fetch_image(url):
+    """fetches the image to a temporary location.
+    Returns that location."""
+    target = tempfile.mkstemp(prefix='bingbg')[1]
+    urllib.urlretrieve(url, target)
+    return target
+
+
+def set_image(filename):
+    cmd = set_image_cmd(filename)
+    ret = subprocess.call(cmd)
+    if ret:
+        raise RuntimeError("Something went wrong when executing %r", cmd)
+
+
+def main():
+    data = get_current_image_data()
+    url = 'http://bing.com/' + data['url']
+    filename = fetch_image(url)
+    set_image(filename)
+
+if __name__ == "__main__":
+    main()
